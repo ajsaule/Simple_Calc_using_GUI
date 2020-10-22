3388d2f397f3fc6bb152319088173e69200e17cb
ajsaule <49604000+ajsaule@users.noreply.github.com>
2020-10-23 10:48:02 +1100

    Fixed syntax error around subtract

--- simple_calculator.py
+++ simple_calculator.py
@@ -34,7 +34,7 @@ def equal():
     if math == "addition":
         e.insert(0, f_num + int(second_number))
 
-    if math == "substraction":
+    if math == "subtraction":
         e.insert(0, f_num - int(second_number))
 
     if math == "multiplication":
@@ -48,7 +48,7 @@ def substract():
     first_number = e.get()
     global f_num
     global math
-    math = "substraction"
+    math = "subtraction"
     f_num = int(first_number)
     e.delete(0, END)
 
@@ -85,7 +85,7 @@ button_add = Button(root, text="+", padx=39, pady=20, command=add)
 button_equal = Button(root, text="=", padx=91, pady=20, command=equal)
 button_clear = Button(root, text="Clear", padx=79, pady=20, command=clear)
 
-button_substract = Button(root, text="-", padx=41, pady=20, command=substract)
+button_subtract = Button(root, text="-", padx=41, pady=20, command=subtract)
 button_multiply = Button(root, text="*", padx=40, pady=20, command=multiply)
 button_divide = Button(root, text="/", padx=41, pady=20, command=divide)
 # put buttons on screen
@@ -107,7 +107,7 @@ button_clear.grid(row=4, column=1, columnspan=2)
 button_add.grid(row=5, column=0)
 button_equal.grid(row=5, column=1, columnspan=2)
 
-button_substract.grid(row=6, column=0)
+button_subtract.grid(row=6, column=0)
 button_multiply.grid(row=6, column=1)
 button_divide.grid(row=6, column=2)
 
